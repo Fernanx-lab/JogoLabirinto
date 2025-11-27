2f91167545b4d9a2ece036e19008ba1e0b83525b
Fernando-Felipe <nandomega11@gmail.com>
2025-11-27 16:40:40 -0300

    aumento de pastas que o git deve ignorar

--- mapa.hpp
+++ mapa.hpp
@@ -1,10 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-char mapa[10][10]; 
+typedef struct {
+    int baus;
+    int curas;
+    int bombas;
+    int chaves;
+}MapaItens;
+
+typedef struct {
+    vector<string> mapaDesenho;
+    MapaItens itens;
+}Mapa;
 
 ifstream SelecaoDeMapa();
-
+Mapa ObtemMapa();
 
 
 
@@ -12,24 +22,40 @@ ifstream SelecaoDeMapa()
 {
     srand(time(NULL));
     int numeroDoMapa = rand() % 3;
-    string nomeDoArquivo = "mapa " + to_string(numeroDoMapa) + ".txt";
+    if(numeroDoMapa == 0)
+    {
+        numeroDoMapa = 1;
+    }
+    string nomeDoArquivo = "mapas/mapa " + to_string(numeroDoMapa) + ".txt";
     ifstream mapa;
     mapa.open(nomeDoArquivo);
+    if(!mapa.is_open())
+    {
+        cout << "Erro ao abrir o arquivo do mapa!" << endl;
+        exit(1);
+    }
     return mapa;
 }
 
-inline vector<string> LerMapa()
+Mapa ObtemMapa()
 {
-    vector<string> mapa;
+    Mapa mapa;
     ifstream arquivoDoMapa = SelecaoDeMapa();
     arquivoDoMapa.ignore(numeric_limits<streamsize>::max(), '\n');
     arquivoDoMapa.ignore(numeric_limits<streamsize>::max(), '\n');
+    mapa.itens.baus = 0;
     
     while(!arquivoDoMapa.eof())
     {
         string linha;
         getline(arquivoDoMapa, linha);
-        mapa.push_back(linha);
+        mapa.itens.baus += count(linha.begin(), linha.end(), 'B');
+        mapa.mapaDesenho.push_back(linha);
     }
     return mapa;
+}
+
+void GerarItensMapa(Mapa& mapa)
+{
+    
 }
